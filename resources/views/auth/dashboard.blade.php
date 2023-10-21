d2751190863891f6ddb9cdc4fc568fa46f6e7eb9
MayCleanSitepu <113108027+MayCleanSitepu@users.noreply.github.com>
2023-10-21 19:46:25 +0700

    first commit

---
+++ resources/views/auth/dashboard.blade.php
@@ -0,0 +1,61 @@
+@extends('auth.layouts')
+
+@section('content')
+
+
+<div id="resume">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/james-moriarty.jpg" alt="James Moriarty">
+        <h1>May Clean Sitepu</h1>
+        <p>Cell: <a href=#>0813-9020-6866</a>
+        <p>Web: <a href=#>maycleans.my.id</a>
+        <p>Email: <a href=#>mayclean0204@mail.ugm.ac.id</a>
+        <p id="objective">I am a reserved but ambitious young professional seeking a career that fits my professional
+            skills, personality, and murderous tendencies. My good birth, excellent education and phenomenal
+            mathematical faculty have allowed me to advance the prospects of several criminal enterprises.
+        <dl>
+            <dt>Education
+            <dd>
+                <h2>Universitas Gadjah Mada</h2>
+                <p><strong>Major:</strong> Vocation in Major Software Engineering<br />
+                    <strong>Minor:</strong> Romance Languages
+                </p>
+        </dl>
+        <dl>
+            <dt>Skills
+            <dd>
+                <h2>Office Skills</h2>
+                <p>Office and records management, database administration, event organization, customer support, travel
+                    coordination
+                <h2>Computer skills</h2>
+                <p>Microsoft productivity software (Word, Excel, etc), Adobe Creative Suite, Windows
+        </dl>
+        <dl>
+            <dt>Experience
+            <dd>
+                <h2>Consulting Criminal<span>London 1892 – present</span></h2>
+                <ul>
+                    <li>Development within the criminal underworld
+                    <li>Conducted negotiations with several rogue governments
+                </ul>
+                <h2>The Coldstream Guards<span>Army Coach, London 1889 – 1888</span></h2>
+                <ul>
+                    <li>Recruiting, training and terrorizing young men.
+                </ul>
+                <h2>Oxford University<span>Professor of Mathematics 1885 – 1888</span></h2>
+                <ul>
+                    <li>Published papers in binomials, asteroid dynamics and game theory
+                    <li>Intimidated students
+                </ul>
+            </dd>
+        </dl>
+        <dl>
+            <dt>Hobbies
+            <dd>World domination, Social Manipulation, Murder Most Foul
+        </dl>
+        <dl>
+            <dt>References
+            <dd>Available on request
+        </dl>
+    </div>
+    
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginRegister;
+use App\Http\Controllers\cv;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(LoginRegister::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+});
+
+Route::get('/cv', [cv::class,'index']);
+
+ 
